59258ad0700ce336c3a9decd5d71122413671b2e
Ruita Wachira <57678615+Charlo-tech@users.noreply.github.com>
2022-04-18 16:59:44 +0300

    update print.c

--- print.c
+++ print.c
@@ -1,49 +1,79 @@
 #include "main.h"
 
 /**
- * _print - writes the character c to stdout 100 charcter
- * @str: The character to print
- *
- * Return: On success number os read.
- * On error, -1 is returned, and errno is set appropriately.
- */
+  * _printf - fills memory with a constant byte
+  * @format: is the string given
+  * Return: numbers of characters printed
+  */
 
-int _printf (char *str, ...)
+int _printf(const char *format, ...)
 {
-        va_list vl;
-        int  i = 0, j = 0;
-        char buff[100] = {0};
-        char *str_arg;
+	int i = 0, cont = 0;
+	va_list va;
+	int (*pp)();
 
-        va_start(vl, str);
-        while (str && str[i])
-        {
-                if (str[i] == '%')
-                {
-                        i++;
-                        switch (str[i])
-                        {
-                        case 'c':{
-                                buff[j] = (char)va_arg(vl, int);
-                                j++;
-                                break;
-                        }
-                        case 's': {
-                                str_arg = va_arg(vl, char*);
-                                strcpy(&buff[j], str_arg);
-                                j += strlen(str_arg);
-                                break;
-                        }
-                        }
+	if (format == NULL || (format[0] == '%' && format[1] == '\0'))
+		return (-1);
+	va_start(va, format);
+	for (i = 0; format != NULL && format[i] != '\0'; i++)
+	{
+		if (format[i] == '%')
+		{
+			pp = equal(format + i);
+			if (pp != NULL)
+			{
+				cont += pp(va);
+				i++;
+			}
+			else
+					cont += _putc(format[i]);
 		}
-                        else
-                        {
-                                buff[j] = str[i];
-                                j++;
-                        }
-                        i++;
-                }
-                write(j, buff, i);
-                va_end(vl);
-                return (j);
+		else
+			cont += _putc(format[i]);
+	}
+	cont += _write(-1, "c", 1);
+	va_end(va);
+	return (cont);
+}
+
+/**
+  * _putc - fills memory with a constant byte
+  * @a: the value to print
+  * Return: numbers of characters printed
+  */
+int _putc(char a)
+{
+	return (_write(1, &(a), 1));
+}
+
+/**
+  * equal - fills memory with a constant byte
+  * @format: the value to print
+  * Return: numbers of characters printed
+  */
+int (*equal(const char *format))()
+{
+	int j;
+	op_t o[] = {
+		{"c", ch},
+		{"s", st},
+		{"i", inte},
+		{"d", inte},
+		{"%", por},
+		{"b", bina},
+		{"o", octa},
+		{"u", unsig},
+		{"x", hexal},
+		{"X", hexau},
+		{"S", sst},
+		{"p", point},
+		{"r", reve},
+		{"R", rot13},
+		{NULL, NULL},
+	};
+
+	for (j = 0; o[j].op != NULL; j++)
+		if (format[1] == o[j].op[0])
+			return (o[j].f);
+	return (o[j].f);
 }
